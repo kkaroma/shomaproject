13e4856531f238cfe910210d3601248247b24afe
Kaizilege Karoma <kkaroma@gmail.com>
2022-02-19 23:43:17 +0300

    try redirect if no image attached

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -52,6 +52,7 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
 
+
     respond_to do |format|
       format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
       format.json { head :no_content }
