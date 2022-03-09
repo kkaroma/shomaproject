a9988e4a9b942740381bd6eb773539b3f694396f
Kaizilege Karoma <kkaroma@gmail.com>
2022-03-09 18:01:00 +0300

    Revert "Show books on category" This reverts commit 56a9bed9305d7e8ae197dc5160d381083ec8a2b4.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,7 +8,6 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1 or /categories/1.json
   def show
-    @books_category = Book.where(category_id: set_category)
   end
 
   # GET /categories/new
