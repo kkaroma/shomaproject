ea53f412e130a10851aa8213fd5e4eca3d9cc618
Kaizilege Karoma <kkaroma@gmail.com>
2022-03-13 18:45:49 +0300

    some comit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   has_rich_text :body
-  has_one_attached :image, service: :amazon_production
+  has_one_attached :image
   validates :title, :author, presence: true
   validates :accno, presence: true, uniqueness: true
   # validates :accno, presence: true, uniqueness: { scope: :user_id }
